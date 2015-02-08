147bd8b589e60d67d3d592eeae7500d766cddc11
iplyac <iplyac@rocketsoftwate.com>
2015-02-09 00:29:43 +0600

    qqq1

--- src/main/java/com/cz/drive/DriveSample1.java
+++ src/main/java/com/cz/drive/DriveSample1.java
@@ -7,10 +7,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.media.MediaHttpDownloader;
-import com.google.api.client.googleapis.media.MediaHttpUploader;
-import com.google.api.client.http.FileContent;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -18,22 +15,20 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.ChildList;
-import com.google.api.services.drive.model.ChildReference;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.ChildReference;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.*;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+public class DriveSample1 {
 
-public class DriveSample {
-
-    private static final String APPLICATION_NAME = "DriveSample1";
-
+    private static final String APPLICATION_NAME = "QA-reltio";
     private static final String FILE_NAME = "cz.cfg";
     private static final java.io.File MY_DRIVE_FILE = new java.io.File(FILE_NAME);
-
     private static final String UPLOAD_FILE_PATH = "C:\\Users\\Иосиф\\Downloads\\ERROR.JPG";
     private static final String DIR_FOR_DOWNLOADS = "C:\\Users\\Public\\Downloads\\";
     private static final java.io.File UPLOAD_FILE = new java.io.File(UPLOAD_FILE_PATH);
@@ -44,26 +39,70 @@ public class DriveSample {
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static Drive drive;
     private static final List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/drive");
+    /*private static HashMap<String, String> DriveIdEntries = new HashMap<String, String>();*/
+    private static HashMap<String, String> DriveNameEntries = new HashMap<String, String>();
+
 
     private static Credential authorize() throws Exception {
-        // load client secrets
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
                 new InputStreamReader(DriveSample.class.getResourceAsStream("/client_secret.json")));
-/*        if (clientSecrets.getDetails().getClientId().startsWith("Enter")
-                || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
-            System.out.println(
-                    "Enter Client ID and Secret from https://code.google.com/apis/console/?api=drive "
-                            + "into drive-cmdline-sample/src/main/resources/client_secrets.json");
-            System.exit(1);
-        }*/
-        // set up authorization code flow
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 httpTransport, JSON_FACTORY, clientSecrets,
                 Collections.singleton(DriveScopes.DRIVE_FILE)).setDataStoreFactory(dataStoreFactory)
                 .setScopes(SCOPES)
                 .build();
-        // authorize
         return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+
+    public static void makeFileMap(Drive service)
+    {
+        /*DriveIdEntries.put("root", "root");*/
+        DriveNameEntries.put("root", "root");
+        try {
+            FileList files = service.files().list().execute();
+            for (File file : files.getItems()) {
+                /*DriveIdEntries.put(file.getTitle(), file.getId());*/
+                DriveNameEntries.put(file.getId(), file.getTitle());
+            }
+        }
+        catch (IOException e){e.printStackTrace();}
+    }
+
+    /**
+     * Get name by ID
+     * @param service
+     * @param fileID
+     * @return
+     */
+    public static  String getNameByID(Drive service, String fileID)
+    {
+        String fileName = null;
+        try {
+            FileList files = service.files().list().execute();
+            for (File file : files.getItems())
+                if (file.getId().equals(fileID)) return file.getTitle();
+        }
+        catch (IOException e){e.printStackTrace();}
+
+        return fileName;
+    }
+
+    /**
+     * Get File id by file name
+     * @param service
+     * @param filename
+     * @return
+     */
+    private static String getFileID(Drive service, String filename)
+    {
+        String fileID = null;
+        try {
+            FileList files = service.files().list().execute();
+            for (File file : files.getItems())
+                if (file.getTitle().equals(filename)) return file.getId();
+        }
+        catch (IOException e){e.printStackTrace();}
+        return fileID;
     }
 
     public static void main(String[] args) {
@@ -74,48 +113,17 @@ public class DriveSample {
             drive = new Drive.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(
                     APPLICATION_NAME).build();
 
-            ChildList folders = drive.children().list("root").execute();
-            for (ChildReference child:folders.getItems())
-                System.out.println(child.getId());
-            downloadFile(true, drive.files().get(getFileID(drive, FILE_NAME)).execute());
+            makeFileMap(drive);
+            String fileID = getFileID("/folder2/new.txt");
 
-            /*File uploadedFile = uploadFile(false);
-            File updatedFile = updateFileWithTestSuffix(uploadedFile.getId());
-            downloadFile(false, updatedFile);
-            uploadedFile = uploadFile(true);
+            downloadFile(true, drive.files().get(fileID).execute());
 
-
-//            downloadFile(true, file);
-            downloadFile(true, uploadedFile);
-
-            return;*/
         } catch (IOException e) {
             System.err.println(e.getMessage());
         } catch (Throwable t) {
             t.printStackTrace();
         }
         System.exit(1);
-    }
-
-    private static File uploadFile(boolean useDirectUpload) throws IOException {
-        File fileMetadata = new File();
-        fileMetadata.setTitle(UPLOAD_FILE.getName());
-
-        FileContent mediaContent = new FileContent("image/jpeg", UPLOAD_FILE);
-
-        Drive.Files.Insert insert = drive.files().insert(fileMetadata, mediaContent);
-        MediaHttpUploader uploader = insert.getMediaHttpUploader();
-        uploader.setDirectUploadEnabled(useDirectUpload);
-        uploader.setProgressListener(new FileUploadProgressListener());
-        return insert.execute();
-    }
-
-    private static File updateFileWithTestSuffix(String id) throws IOException {
-        File fileMetadata = new File();
-        fileMetadata.setTitle("drivetest-" + UPLOAD_FILE.getName());
-
-        Drive.Files.Update update = drive.files().update(id, fileMetadata);
-        return update.execute();
     }
 
     private static void downloadFile(boolean useDirectDownload, File uploadedFile)
@@ -134,39 +142,34 @@ public class DriveSample {
         downloader.download(new GenericUrl(uploadedFile.getDownloadUrl()), out);
     }
 
-    private static InputStream downloadFile(Drive service, File file) {
-        if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
-            try {
-                HttpResponse resp =
-                        service.getRequestFactory().buildGetRequest(new GenericUrl(file.getDownloadUrl()))
-                                .execute();
-                return resp.getContent();
-            } catch (IOException e) {
-                // An error occurred.
-                e.printStackTrace();
-                return null;
-            }
-        } else {
-            // The file doesn't have any content stored on Drive.
-            return null;
+    private static String getFileID(String path) {
+
+        String[] folders = path.replaceFirst("^/", "").split("/");
+        String currentEntryID = "root";
+        for (int nesting = 0; nesting < folders.length; nesting++)
+        {
+            List<String> filesIDInCurrentDirectory = getFilesIDInFolder(currentEntryID);
+            for (String fileID:filesIDInCurrentDirectory)
+                if (getNameByID(drive, fileID).equals(folders[nesting])){
+                    currentEntryID = fileID;
+                    break;
+                }
         }
+
+        return currentEntryID;
     }
 
-    /**
-     * Get File id by file name
-     * @param service
-     * @param filename
-     * @return
-     */
-    private static String getFileID(Drive service, String filename)
+    private static List<String> getFilesIDInFolder(String fileID)
     {
-        String fileID = null;
-        try {
-            FileList files = service.files().list().execute();
-            for (File file : files.getItems())
-                if (file.getTitle().equals(filename))return file.getId();
-        }
-        catch (IOException e){e.printStackTrace();}
-        return fileID;
+        List<String> fileIDList = new ArrayList<String>();
+        try
+        {
+            ChildList folderEntries = drive.children().list(fileID).execute();
+            for (ChildReference child:folderEntries.getItems())
+                fileIDList.add(child.getId() + "");
+        }catch(IOException e)
+              {e.printStackTrace();}
+        return fileIDList;
     }
+
 }
